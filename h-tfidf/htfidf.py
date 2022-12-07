66a01828c85bf79e84bf592c09463704238862af
Decoupes Remy <remy.decoupes@irstea.fr>
2022-12-07 18:37:17 +0100

    prepare python import

--- h-tfidf/htfidf.py
+++ h-tfidf/htfidf.py
@@ -18,10 +18,14 @@ def h_tfidf_higgest_scores(data: pd.DataFrame,
         - list_ranked_terms: list of ranked terms for the geo-period window
         - list_rannked_scores: list of H-TFIDF scores
     """
+    # Check args
     list_spatial_level = ["city", "state", "country"]
     list_temporal_level = ["day", "week", "month", "year"]
     if spatial_level not in list_spatial_level:
         raise ValueError(f"Wrong spatial level, please select a value from: {list_spatial_level}")
     if temporal_level not in list_temporal_level:
         raise ValueError(f"Wrong temporal level, please select value from list: {list_temporal_level}")
+    if "text" or "geo" or "period" not in data:
+        raise ValueError(f"Data are badly formatted. Column names needed: text, geo and period ")
+
 
